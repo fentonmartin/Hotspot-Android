90725336ca40ddda30e7c74b246bb5751c4d36e8
Fenton Martin <fentonxm@gmail.com>
2018-12-04 23:38:57 +0700

    Add permission requests on MainActivity

--- app/src/main/java/com/knexis/hotspotexample/MainActivity.java
+++ app/src/main/java/com/knexis/hotspotexample/MainActivity.java
@@ -1,7 +1,19 @@
 package com.knexis.hotspotexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -24,10 +36,55 @@ public class MainActivity extends AppCompatActivity implements HotspotListener{
 
         tvDevicesConnected = findViewById(R.id.tv_devices_connected);
 
-        hotspot = new Hotspot(this);
+        setPermission();
 
+        hotspot = new Hotspot(this);
         hotspot.startListener(this);
 
+    }
+
+    private void setPermission() {
+        youDesirePermissionCode(this);
+    }
+
+    static int CODE_WRITE_SETTINGS_PERMISSION = 101;
+
+    public static void youDesirePermissionCode(Activity context){
+        boolean permission;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permission = Settings.System.canWrite(context);
+        } else {
+            permission = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_SETTINGS) == PackageManager.PERMISSION_GRANTED;
+        }
+        if (permission) {
+            //do your code
+        }  else {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + context.getPackageName()));
+                context.startActivityForResult(intent, MainActivity.CODE_WRITE_SETTINGS_PERMISSION);
+            } else {
+                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.WRITE_SETTINGS}, MainActivity.CODE_WRITE_SETTINGS_PERMISSION);
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MainActivity.CODE_WRITE_SETTINGS_PERMISSION && Settings.System.canWrite(this)){
+            Log.d("TAG", "MainActivity.CODE_WRITE_SETTINGS_PERMISSION success");
+            //do your code
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MainActivity.CODE_WRITE_SETTINGS_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //do your code
+        }
     }
 
     public void OnStartHotspotClicked(View view) {
